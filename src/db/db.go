fc48c7ff7887dede74df55a7d42fdbc5b3ace9bb
Leng-Kai <b07705049@ntu.edu.tw>
2021-06-30 22:04:43 +0800

    [User] Implement db operation interface functions

--- src/db/db.go
+++ src/db/db.go
@@ -6,8 +6,10 @@ import (
 
 var db *mongo.Client
 var courses *mongo.Collection
+var users *mongo.Collection
 
 func InitDB(client *mongo.Client) {
 	db = client
 	courses = db.Database("CourseDB").Collection("courses")
+	users = db.Database("CourseDB").Collection("users")
 }
