18e39333d6a9975afd998fc5488c79ec9374c50d
Leng-Kai <b07705049@ntu.edu.tw>
2021-08-08 22:45:40 +0800

    Customize helper function "Contain()" for different types

--- src/util/array.go
+++ src/util/array.go
@@ -1,7 +1,20 @@
 package util
 
-func Contain(array []interface{}, item interface{}) bool {
-	for i, ele := range array {
+import (
+	"github.com/Leng-Kai/bow-code-API-server/schemas"
+)
+
+func Contain_str(array []string, item string) bool {
+	for _, ele := range array {
+		if ele == item {
+			return true
+		}
+	}
+	return false
+}
+
+func Contain_ID(array []schemas.ID, item schemas.ID) bool {
+	for _, ele := range array {
 		if ele == item {
 			return true
 		}
