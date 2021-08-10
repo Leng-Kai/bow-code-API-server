cb0cf6566d32a82748d2a8c2ff0027636ed18dcd
Leng-Kai <b07705049@ntu.edu.tw>
2021-08-10 15:46:42 +0800

    [MongoDB] Migrate data into single DB

--- src/db/db.go
+++ src/db/db.go
@@ -4,6 +4,8 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+const db_name = "bow-code-DB"
+
 var db *mongo.Client
 var courses *mongo.Collection
 var courseplans *mongo.Collection
@@ -11,15 +13,15 @@ var classrooms *mongo.Collection
 var users *mongo.Collection
 var problems *mongo.Collection
 var submissions *mongo.Collection
-var Session *mongo.Collection
+// var Session *mongo.Collection
 
 func InitDB(client *mongo.Client) {
 	db = client
-	courses = db.Database("CourseDB").Collection("courses")
-	courseplans = db.Database("CourseDB").Collection("courseplans")
-	classrooms = db.Database("CourseDB").Collection("classrooms")
-	users = db.Database("UserDB").Collection("users")
-	problems = db.Database("ProblemDB").Collection("problems")
-	submissions = db.Database("SubmissionDB").Collection("submissions")
-	Session = db.Database("SessionDB").Collection("sessions")
+	courses = db.Database(db_name).Collection("courses")
+	courseplans = db.Database(db_name).Collection("courseplans")
+	classrooms = db.Database(db_name).Collection("classrooms")
+	users = db.Database(db_name).Collection("users")
+	problems = db.Database(db_name).Collection("problems")
+	submissions = db.Database(db_name).Collection("submissions")
+	// Session = db.Database("SessionDB").Collection("sessions")
 }
