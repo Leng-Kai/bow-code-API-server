38c4d40cd8869ba240983d673e78b8512281e568
lin <b07705049@ntu.edu.tw>
2021-08-17 16:05:03 +0800

    [CoursePlan] Generalize the component of course plan

--- src/schemas/courseplan_schema.go
+++ src/schemas/courseplan_schema.go
@@ -6,11 +6,16 @@ import (
 
 type CoursePlanID = ID
 
+type CoursePlanComponent struct {
+	Type int `json:"type" bson:"type"`
+	ID   ID  `json:"id" bson:"id"`
+}
+
 type CoursePlan struct {
-	CoursePlanID CoursePlanID `json:"id" bson:"_id,omitempty"`
-	Name         string       `json:"name" bson:"name"`
-	CourseList   []CourseID   `json:"courseList" bson:"courseList"`
-	Creator      UserID       `json:"creator" bson:"creator"`
-	Visibility   int          `json:"visibility" bson:"visibility"`
-	CreateTime   time.Time    `json:"createTime" bson:"createTime"`
+	CoursePlanID  CoursePlanID          `json:"id" bson:"_id,omitempty"`
+	Name          string                `json:"name" bson:"name"`
+	ComponentList []CoursePlanComponent `json:"componentList" bson:"componentList"`
+	Creator       UserID                `json:"creator" bson:"creator"`
+	Visibility    int                   `json:"visibility" bson:"visibility"`
+	CreateTime    time.Time             `json:"createTime" bson:"createTime"`
 }
