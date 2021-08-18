e43ca6238ae5dd4cd98e0b3e5817b42fba8a0ed5
lin <b07705049@ntu.edu.tw>
2021-08-18 18:53:36 +0800

    [ClassroomRecord] Implement db operation functions

--- src/db/db.go
+++ src/db/db.go
@@ -10,6 +10,7 @@ var db *mongo.Client
 var courses *mongo.Collection
 var courseplans *mongo.Collection
 var classrooms *mongo.Collection
+var classroomrecords *mongo.Collection
 var users *mongo.Collection
 var problems *mongo.Collection
 var submissions *mongo.Collection
@@ -20,6 +21,7 @@ func InitDB(client *mongo.Client) {
 	courses = db.Database(db_name).Collection("courses")
 	courseplans = db.Database(db_name).Collection("courseplans")
 	classrooms = db.Database(db_name).Collection("classrooms")
+	classroomrecords = db.Database(db_name).Collection("classroomrecords")
 	users = db.Database(db_name).Collection("users")
 	problems = db.Database(db_name).Collection("problems")
 	submissions = db.Database(db_name).Collection("submissions")
