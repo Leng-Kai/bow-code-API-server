5686c9ea7c268e0a30613a29e94a1badd9d2811f
Leng-Kai <b07705049@ntu.edu.tw>
2021-09-17 19:41:59 +0800

    [User] Add SchoolInfo field in user schema

--- src/schemas/user_schema.go
+++ src/schemas/user_schema.go
@@ -8,23 +8,33 @@ type UserInfo struct {
 	Email  string `json:"email" bson:"email"`
 }
 
-type User struct {
-	UserID               `json:"id" bson:"_id,omitempty"`
-	RegisterType         string `json:"registerType" bson:"registerType"`
-	UserInfo             `json:"userInfo" bson:"userInfo"`
-	Super                bool           `json:"super" bson:"super"`
+type SchoolInfo struct {
+	SchoolName  string `json:"schoolName" bson:"schoolName"`
+	StudentID   string `json:"studentID" bson:"studentID"`
+	StudentName string `json:"studentName" bson:"studentName"`
+	Class       string `json:"class" bson:"class"`
+	SeatNumber  int    `json:"seatNumber" bson:"seatNumber"`
+}
 
-	JoinedCourseList     []CourseID     `json:"joinedCourseList" bson:"joinedCourseList"`
-	OwnCourseList        []CourseID     `json:"ownCourseList" bson:"ownCourseList"`
-	FavoriteCourseList   []CourseID     `json:"favoriteCourseList" bson:"favoriteCourseList"`
+type User struct {
+	UserID              UserID     `json:"id" bson:"_id,omitempty"`
+	RegisterType        string     `json:"registerType" bson:"registerType"`
+	UserInfo            UserInfo   `json:"userInfo" bson:"userInfo"`
+	SchoolInfo          SchoolInfo `json:"schoolInfo" bson:"schoolInfo"`
+	SchoolInfoCompleted bool       `json:"schoolInfoCompleted" bson:"schoolInfoCompleted"`
+	Super               bool       `json:"super" bson:"super"`
+
+	JoinedCourseList   []CourseID `json:"joinedCourseList" bson:"joinedCourseList"`
+	OwnCourseList      []CourseID `json:"ownCourseList" bson:"ownCourseList"`
+	FavoriteCourseList []CourseID `json:"favoriteCourseList" bson:"favoriteCourseList"`
 
 	JoinedCoursePlanList []CoursePlanID `json:"joinedCoursePlanList" bson:"joinedCoursePlanList"`
 	OwnCoursePlanList    []CoursePlanID `json:"ownCoursePlanList" bson:"ownCoursePlanList"`
 
-	OwnProblemList       []ProblemID    `json:"ownProblemList" bson:"ownProblemList"`
-	FavoriteProblemList  []ProblemID    `json:"favoriteProblemList" bson:"favoriteProblemList"`
+	OwnProblemList      []ProblemID `json:"ownProblemList" bson:"ownProblemList"`
+	FavoriteProblemList []ProblemID `json:"favoriteProblemList" bson:"favoriteProblemList"`
 
-	JoinedClassroomList  []ClassroomID  `json:"joinedClassroomList" bson:"joinedClassroomList"`
-	OwnClassroomList     []ClassroomID  `json:"ownClassroomList" bson:"ownClassroomList"`
-	AppliedClassroomList []ClassroomID  `json:"appliedClassroomList" bson:"appliedClassroomList"`
+	JoinedClassroomList  []ClassroomID `json:"joinedClassroomList" bson:"joinedClassroomList"`
+	OwnClassroomList     []ClassroomID `json:"ownClassroomList" bson:"ownClassroomList"`
+	AppliedClassroomList []ClassroomID `json:"appliedClassroomList" bson:"appliedClassroomList"`
 }
