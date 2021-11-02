ba256cb69bf1b8902274d6990898ae9865ab0b55
Leng-Kai <b07705049@ntu.edu.tw>
2021-11-02 12:30:51 +0000

    [Problem] Modify DefaultContent schema

--- src/schemas/problem_schema.go
+++ src/schemas/problem_schema.go
@@ -14,19 +14,24 @@ type Testcase struct {
 	ShowDetail     []bool   `json:"showDetail" bson:"showDetail"`
 }
 
+type Contents struct {
+	Language int    `json:"language" bson:"language"`
+	Content  string `json:"content" bson:"content"`
+}
+
 type Problem struct {
-	ProblemID      ProblemID      `json:"id" bson:"_id,omitempty"`
-	Name           string         `json:"name" bson:"name"`
-	Creator        UserID         `json:"creator" bson:"creator"`
-	Description    string         `json:"description" bson:"description"`
-	DefaultContent map[int]string `json:"defaultContent" bson:"defaultContent"`
-	Testcase       Testcase       `json:"testcase" bson:"testcase"`
-	TotalScore     int            `json:"totalScore" bson:"totalScore"`
-	Tags           []string       `json:"tags" bson:"tags"`
-	Difficulty     int            `json:"difficulty" bson:"difficulty"`
-	Category       string         `json:"category" bson:"category"`
-	Visibility     int            `json:"visibility" bson:"visibility"`
-	CreateTime     time.Time      `json:"createTime" bson:"createTime"`
+	ProblemID      ProblemID  `json:"id" bson:"_id,omitempty"`
+	Name           string     `json:"name" bson:"name"`
+	Creator        UserID     `json:"creator" bson:"creator"`
+	Description    string     `json:"description" bson:"description"`
+	DefaultContent []Contents `json:"defaultContent" bson:"defaultContent"`
+	Testcase       Testcase   `json:"testcase" bson:"testcase"`
+	TotalScore     int        `json:"totalScore" bson:"totalScore"`
+	Tags           []string   `json:"tags" bson:"tags"`
+	Difficulty     int        `json:"difficulty" bson:"difficulty"`
+	Category       string     `json:"category" bson:"category"`
+	Visibility     int        `json:"visibility" bson:"visibility"`
+	CreateTime     time.Time  `json:"createTime" bson:"createTime"`
 }
 
 type TagCount struct {
